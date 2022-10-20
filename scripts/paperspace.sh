04c30b0dff01e312cc9b6f19d15843000ba9ceac
Lukáš Málek <malek.luky@gmail.com>
2022-10-21 00:23:49 +0200

    Fixed ref_mesh path

--- scripts/paperspace.sh
+++ scripts/paperspace.sh
@@ -12,8 +12,10 @@ pip install git+https://github.com/NVlabs/nvdiffrast/
 pip install --global-option="--no-networks" git+https://github.com/NVlabs/tiny-cuda-nn#subdirectory=bindings/torch
 git clone https://github.com/NVlabs/nvdiffrec.git
 git clone https://github.com/malek-luky/Motionshift-PaperSpace.git
-cp C:/Users/paperspace/Desktop//Motionshift-PaperSpace/test_fox/test_fox.json C:/Users/paperspace/Desktop/nvdiffrec/configs/
+cp C:/Users/paperspace/Desktop/Motionshift-PaperSpace/test_fox.json C:/Users/paperspace/Desktop/nvdiffrec/configs/
 
+python C:/Users/paperspace/Desktop/nvdiffrec/train.py --config C:/Users/paperspace/Desktop/nvdiffrec/configs/test_fox.json --display-interval 100
+python train.py --config configs/test_fox.json --display-interval 20
 
 
 pip install --upgrade pip
@@ -30,6 +32,7 @@ pip3 install --global-option="--no-networks" git+https://github.com/NVlabs/tiny-
 imageio_download_bin freeimage
 
 ## install git + python(3.9.1!!) from mthe web
-##install visual studio from bicloud
+## install visual studio from bicloud
+## isntall CUDA 11.6 (https://developer.nvidia.com/cuda-11-6-0-download-archive)
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 c:\users\paperspace\appdata\local\programs\python\python39\python.exe -m pip install --upgrade pip
